3f4e8e566f5a2a47a417c087167ae1d49e055f8b
Francisco Jean <71938841+Francisco-Jean@users.noreply.github.com>
2023-06-09 13:07:08 -0300

    Nova API

---
+++ src/main/java/com/example/testesspring/TestesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.testesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesSpringApplication.class, args);
+	}
+
+}
